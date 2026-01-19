caabf1d8bd0014e6a9bbf8e31d9ce4c9986ee4b4
parcidio <parcidioandre@gmail.com>
2026-01-20 00:24:31 +0100

    final before presentation

--- constant/index.ts
+++ constant/index.ts
@@ -6,8 +6,8 @@ export const allBlockLayouts = ["RowLayout", "ColumnLayout", "GridLayout"];
 export const fontWeightClass = {
   normal: "font-normal",
   bold: "font-bold",
-  bolder: "font-extrabold", // Tailwind uses 'font-extrabold' for 'bolder'
-  lighter: "font-extralight", // Tailwind uses 'font-light' for 'lighter'
+  bolder: "font-black", // Tailwind uses 'font-extrabold' for 'bolder'
+  lighter: "font-thin", // Tailwind uses 'font-light' for 'lighter'
 };
 
 export const fontSizeClass = {
@@ -15,6 +15,6 @@ export const fontSizeClass = {
   medium: "text-base", // Tailwind class for medium font
   large: "text-lg", // Tailwind class for large font
   "x-large": "text-xl", // Tailwind class for extra large font
-  "2x-large": "text-2xl",
-  "4x-large": "text-4xl",
+  "2x-large": "text-4xl",
+  "4x-large": "text-6xl",
 };
